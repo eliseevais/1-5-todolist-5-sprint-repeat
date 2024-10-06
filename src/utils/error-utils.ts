e9994b204b8dcb6e27d24ef5021ac8324c0161e3
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2024-10-06 20:20:24 +0300

    sprint05 week01 lesson17: redux tool kit

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,19 @@
+import {setAppErrorAC, setAppStatusAC} from '../app/app-reducer'
+import {ResponseType} from '../api/todolists-api'
+import {Dispatch} from 'redux'
+
+// export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
+  if (data.messages.length) {
+    dispatch(setAppErrorAC({error: data.messages[0]}))
+  } else {
+    dispatch(setAppErrorAC({error: 'Some error occurred'}))
+  }
+  dispatch(setAppStatusAC({status: 'failed'}))
+}
+
+// export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch) => {
+  dispatch(setAppErrorAC({error: error.message ? error.message : 'Some error occurred'}))
+  dispatch(setAppStatusAC({status: 'failed'}))
+}
